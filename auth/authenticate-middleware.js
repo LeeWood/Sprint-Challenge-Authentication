cbad5ca23d176b91fee057e5680defc9e0a1d404
lee-wood <leemaewood@gmail.com>
2020-02-21 22:30:21 -0500

    middleware implemented, user must have active token to access joke endpoint

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,27 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secrets = require('./secrets.js');
+const User = require('../users/users-model.js');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+
+  const token = req.headers.authorization;
+
+  if(req.decodedJwt) {
+    next();
+  }else if(token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedJwt) => {
+      if(err) {
+        res.status(500).json({
+          message: "There's been an error",
+          error: err
+        })
+      }else {
+        req.decodedJwt = decodedJwt;
+        next();
+      }
+    })
+  }else {
+    res.status(401).json({ you: 'shall not pass!' });
+  }
 };
